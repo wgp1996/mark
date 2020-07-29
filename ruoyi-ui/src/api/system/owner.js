5adb1fa40ba54664b7f38b2bce6b7a16e77a24e1
wgp1996 <1782272017@qq.com>
2020-07-29 13:49:15 +0800

    添加业户信息

---
+++ ruoyi-ui/src/api/system/owner.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询业户信息列表
+export function listOwner(query) {
+  return request({
+    url: '/system/owner/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询业户信息详细
+export function getOwner(id) {
+  return request({
+    url: '/system/owner/' + id,
+    method: 'get'
+  })
+}
+
+// 新增业户信息
+export function addOwner(data) {
+  return request({
+    url: '/system/owner',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改业户信息
+export function updateOwner(data) {
+  return request({
+    url: '/system/owner',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除业户信息
+export function delOwner(id) {
+  return request({
+    url: '/system/owner/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出业户信息
+export function exportOwner(query) {
+  return request({
+    url: '/system/owner/export',
+    method: 'get',
+    params: query
+  })
+}
