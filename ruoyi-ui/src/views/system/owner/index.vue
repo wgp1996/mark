fbc0190b55946d0a5b73b80ad129c1ccf6591849
wgp1996 <1782272017@qq.com>
2020-07-29 16:03:10 +0800

    删除列表ID

--- ruoyi-ui/src/views/system/owner/index.vue
+++ ruoyi-ui/src/views/system/owner/index.vue
@@ -68,7 +68,6 @@
 
     <el-table v-loading="loading" :data="ownerList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="id" align="center" prop="id" />
       <el-table-column label="业主编号" align="center" prop="ownerCode" />
       <el-table-column label="业主名称" align="center" prop="ownerName" />
       <el-table-column label="组织类型" align="center" prop="ownerOrg" />
