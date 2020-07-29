634bcfa52fd5e8336bd75de27e842308c6ddc022
wgp1996 <1782272017@qq.com>
2020-07-29 10:07:38 +0800

    修改接口地址

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://wbt967.natappfree.cc`,
+        target: `http://ximxc2.natappfree.cc`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
