815dccd6eb33564290d58f3441f6bc6f38ead03b
yu <2502436081@qq.com>
2020-09-02 16:12:38 +0800

    提交

--- ruoyi-ui/src/views/dashboard/LineImg.vue
+++ ruoyi-ui/src/views/dashboard/LineImg.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="banner">
       <!--动态将图片轮播图的容器高度设置成与图片一致-->
-      <el-carousel :height="300 + 'px'"  type="card" style="width:100%" arrow="always">
+      <el-carousel   type="card" style="width:100%" arrow="always">
         <!--遍历图片地址,动态生成轮播图-->
-        <el-carousel-item v-for="item in img_list" :key="item" :width="300+'px'">
-          <img :src="item" alt="" style="height:300px;">
+        <el-carousel-item v-for="item in img_list" :key="item.url"  style="text-align:center">
+          <img :src="item.url" alt="" >
+          <!-- <span style="display:inline-block;text-align:center">{{item.price}}</span> -->
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -23,17 +24,45 @@
             //   "/static/image/444.png",
             // ],
             img_list: [
-               "https://www.lczhuisu.cn:443/qdzq/images/a1.jpg",
-               "https://www.lczhuisu.cn:443/qdzq/images/a2.jpg",
-               "https://www.lczhuisu.cn:443/qdzq/images/a3.jpg",
-                "https://www.lczhuisu.cn:443/qdzq/images/a4.jpg",
-                "https://www.lczhuisu.cn:443/qdzq/images/a5.jpg",
+              //  "https://www.lczhuisu.cn:443/qdzq/images/a1.jpg",
+              //  "https://www.lczhuisu.cn:443/qdzq/images/a2.jpg",
+              //  "https://www.lczhuisu.cn:443/qdzq/images/a3.jpg",
+              //   "https://www.lczhuisu.cn:443/qdzq/images/a4.jpg",
+              //   "https://www.lczhuisu.cn:443/qdzq/images/a5.jpg",
+              {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a1.jpg",
+          price:'2.55'
+        },
+        {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a2.jpg",
+           price:'3.50'
+        },
+        {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a3.jpg",
+           price:'2.78'
+        },
+        {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a4.jpg",
+           price:'2.89'
+        },
+        {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a5.jpg",
+           price:'1.85'
+        },
+        {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a6.jpg",
+           price:'2.54'
+        },
+        {
+          url: "https://www.lczhuisu.cn:443/qdzq/images/a8.jpg",
+           price:'2.5'
+        }
               
                
       
         ],
             // 图片父容器高度
-            bannerHeight :300,
+            // bannerHeight :300,
             // 浏览器宽度
             screenWidth :0,
           }
@@ -69,7 +98,7 @@
  .el-carousel__item{
    width: 50% !important;
    text-align: center;
- 
+
 
  }
  
@@ -79,7 +108,10 @@
   }  */
   img{
     /*设置图片宽度和浏览器宽度一致*/
-    width: 55%;
+    width: 300px !important;
+    /* height:250px; */
+    border-radius:50%;
     height: inherit;
+    
   }
 </style>
